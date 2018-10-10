f4ef06aeba9dbf4b23535f38a26e9da4184cdc6d
Matt <sotek222@yahoo.com>
2018-10-10 15:35:27 -0400

    adds basic sketch functionality

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+// document.addEventListener("DOMContentLoaded", () => {
 
 let grid = 16;
 
@@ -8,9 +8,23 @@ let grid = 16;
     for(let i=0; i<(grid*grid); i++){
         const box = document.createElement("div");
         box.classList.add("grid-item");
-        box.textContent = "O";
+        box.setAttribute("name", "block");
+        // box.textContent = "O";
         container.appendChild(box);
     }
   }
   createPad(grid);
-})
+
+  const gridBlock = document.querySelectorAll(".grid-item");
+
+  function hoverChange(e){
+    e.target.style.backgroundColor = "rgb(0,0,0)"
+  };
+
+
+  for(let i=0;i<gridBlock.length;i++){
+    gridBlock[i].addEventListener('mouseover', hoverChange)
+  }
+
+
+// })
